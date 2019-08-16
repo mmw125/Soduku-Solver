fd2e740e19c7b61914d1a207202bad7183c9575c
mmw12 <mmw12@DESKTOP-U7ETOR2>
2019-08-15 22:00:17 -0400

    Format files

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -18,71 +18,64 @@ public class MainPanel {
 	}
 
 	private JFrame frame;
-	public MainPanel()
-	{
-		frame = new JFrame("Spades Scorekeeper");
+
+	public MainPanel() {
+		frame = new JFrame("Soduku Solver");
 		frame.setVisible(true);
 		frame.setSize(300, 325);
 		frame.setMinimumSize(new Dimension(300, 325));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		Container container = frame.getContentPane();
 		container.setLayout(new BorderLayout());
-		
+
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(9, 9, 2, 2));
 		NumberBox.fillPanelWithBoxes(panel);
 		container.add(panel, BorderLayout.CENTER);
-		
+
 		JButton button = new JButton("Solve");
 		button.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				solve();
 			}
 		});
 		container.add(button, BorderLayout.SOUTH);
-		//SpadesBox.setUpPuzzle1();
+		// SpadesBox.setUpPuzzle1();
 		frame.revalidate();
 	}
-	
-	private void solve()
-	{
+
+	private void solve() {
 		System.out.println("Solving");
-		if(NumberBox.isSolved())
-		{
+		if (NumberBox.isSolved()) {
 			printSolvedState();
 		}
 		NumberBox box = NumberBox.boxWithLeastOptions();
-		if(box == null)
-		{
+		if (box == null) {
 			System.out.println("Box == null");
 			return;
 		}
 		ArrayList<Integer> options = box.getOptions();
-		for(Integer i : options)
-		{
+		for (Integer i : options) {
 			System.out.println("Looking at " + i);
 			box.setValue(i);
 			solve();
 		}
 		box.setValue(0);
 	}
-	
-	private void printSolvedState()
-	{
+
+	private void printSolvedState() {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		frame.getContentPane().setLayout(new GridLayout(9, 9));
 		frame.setSize(300, 300);
-	    NumberBox[][] boxes = NumberBox.getBoxes();
-	    for(NumberBox[] list : boxes)
-	    {
-	    	for(NumberBox box : list)
-	    	{
-	    		frame.getContentPane().add(new JLabel(box.getValue()+""));
-	    	}
-	    }
+		NumberBox[][] boxes = NumberBox.getBoxes();
+		for (NumberBox[] list : boxes) {
+			for (NumberBox box : list) {
+				frame.getContentPane().add(new JLabel(box.getValue() + ""));
+			}
+		}
 	}
 }
--- src/NumberBox.java
+++ src/NumberBox.java
@@ -7,49 +7,35 @@ import java.util.ArrayList;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
-public class NumberBox
-{
+public class NumberBox {
 	private static NumberBox[][] boxes;
-	
-	public static boolean isSolved()
-	{
-		for(int x = 0; x < 9; x++)
-		{
-			for(int y = 0; y < 9; y++)
-			{
-				if(boxes[x][y].getValue() == 0)
-				{
+
+	public static boolean isSolved() {
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				if (boxes[x][y].getValue() == 0) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	
-	public static NumberBox boxWithLeastOptions()
-	{
+
+	public static NumberBox boxWithLeastOptions() {
 		NumberBox boxWithLeastOptions = null;
 		int leastOptions = 0;
-		for(int x = 0; x < 9; x++)
-		{
-			for(int y = 0; y < 9; y++)
-			{
-				if(boxes[x][y].getValue() == 0)
-				{
-					if(boxWithLeastOptions == null)
-					{
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				if (boxes[x][y].getValue() == 0) {
+					if (boxWithLeastOptions == null) {
 						boxWithLeastOptions = boxes[x][y];
 						leastOptions = boxWithLeastOptions.options();
-					}
-					else
-					{
+					} else {
 						int options = boxes[x][y].options();
-						if(options == 0)
-						{
+						if (options == 0) {
 							return null;
 						}
-						if(options < leastOptions)
-						{
+						if (options < leastOptions) {
 							boxWithLeastOptions = boxes[x][y];
 						}
 					}
@@ -59,8 +45,7 @@ public class NumberBox
 		return boxWithLeastOptions;
 	}
 
-	public static void setUpPuzzle1()
-	{
+	public static void setUpPuzzle1() {
 		boxes[0][0].setValue(1);
 		boxes[1][1].setValue(2);
 		boxes[2][1].setValue(3);
@@ -89,30 +74,23 @@ public class NumberBox
 		boxes[7][7].setValue(8);
 		boxes[8][8].setValue(7);
 	}
-	
-	public static void fillPanelWithBoxes(Container con)
-	{
+
+	public static void fillPanelWithBoxes(Container con) {
 		getBoxes();
-		for(int x = 0; x < 9; x++)
-		{
-			for(int y = 0; y < 9; y++)
-			{
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
 				con.add(boxes[x][y].getField());
 			}
 		}
 	}
-	
-	public static NumberBox[][] getBoxes()
-	{
-		if(boxes == null)
-		{
+
+	public static NumberBox[][] getBoxes() {
+		if (boxes == null) {
 			boxes = new NumberBox[9][9];
-			for(int x = 0; x < 9; x++)
-			{
-				for(int y = 0; y < 9; y++)
-				{
+			for (int x = 0; x < 9; x++) {
+				for (int y = 0; y < 9; y++) {
 					boxes[x][y] = new NumberBox(new Point(x, y));
-					if((x/3 + y/3) % 2 == 0){
+					if ((x / 3 + y / 3) % 2 == 0) {
 						boxes[x][y].getField().setBackground(Color.DARK_GRAY);
 					}
 				}
@@ -120,89 +98,74 @@ public class NumberBox
 		}
 		return boxes;
 	}
-	
+
 	private Point position;
 	private JSpinner spinner;
-	
-	private NumberBox(Point position)
-	{
+
+	private NumberBox(Point position) {
 		this.position = position;
 		spinner = new JSpinner();
 		spinner.setModel(new SpinnerNumberModel(0, 0, 9, 1));
 	}
-	
-	public int getValue()
-	{
+
+	public int getValue() {
 		return (int) spinner.getValue();
 	}
-	
-	public Component getField()
-	{
+
+	public Component getField() {
 		return spinner;
 	}
-	
+
 	private int options() {
 		int options = 9;
-		for(int i = 1; i <= 9; i++)
-		{
-			for(NumberBox box : relatedTo())
-			{
-				if(box.getValue() == i)
-				{
+		for (int i = 1; i <= 9; i++) {
+			for (NumberBox box : relatedTo()) {
+				if (box.getValue() == i) {
 					options--;
 					break;
 				}
 			}
 		}
-		if(options == 0){
+		if (options == 0) {
 			System.out.println(this + " Returns 0 for options");
 		}
 		return options;
 	}
-	
-	private ArrayList<NumberBox> relatedTo()
-	{
+
+	private ArrayList<NumberBox> relatedTo() {
 		ArrayList<NumberBox> relatedTo = new ArrayList<NumberBox>();
-		for(int x = 0; x < 9; x++)
-		{
-			for(int y = 0; y < 9; y++)
-			{
-				if(position.x == x || position.y == y || (position.x / 3 == x / 3 && position.y / 3 == y / 3))
-				{
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				if (position.x == x || position.y == y || (position.x / 3 == x / 3 && position.y / 3 == y / 3)) {
 					relatedTo.add(boxes[x][y]);
 				}
 			}
 		}
 		return relatedTo;
 	}
-	
+
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		return position.x + ", " + position.y + ": " + getValue();
 	}
-	
-	public void setValue(int value)
-	{
+
+	public void setValue(int value) {
 		spinner.setValue(value);
 		spinner.revalidate();
 	}
 
 	public ArrayList<Integer> getOptions() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i = 1; i <= 9; i++)
-		{
+		for (int i = 1; i <= 9; i++) {
 			list.add(i);
-			for(NumberBox box : relatedTo())
-			{
-				if(box.getValue() == i)
-				{
+			for (NumberBox box : relatedTo()) {
+				if (box.getValue() == i) {
 					list.remove(list.size() - 1);
 					break;
 				}
 			}
 		}
-		if(list.size() == 0){
+		if (list.size() == 0) {
 			System.out.println(this + " Returns 0 for options");
 		}
 		return list;
